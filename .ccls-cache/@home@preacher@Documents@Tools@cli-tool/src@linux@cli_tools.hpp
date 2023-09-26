5434197147adba58725e0735cd0db6e86594e422
PreacherDHM <jesseh2003@outlook.com>
2023-09-26 08:10:23 -0700

    Started adding main functions for the terminal and added pos escape code

--- .ccls-cache/@home@preacher@Documents@Tools@cli-tool/src@linux@cli_tools.hpp
+++ .ccls-cache/@home@preacher@Documents@Tools@cli-tool/src@linux@cli_tools.hpp
@@ -10,46 +10,62 @@ namespace clts {
     
     class pain {
     public:
-        //# pain**(posX, posY, sizeX, sizeY)**
-        //  - **posX** is the X position of the pain *starting at 0*
-        //  - **posY** is the Y position of the pain *starting at 0*
-        //  - **sizeX** is the scail along the X Axis *this has to be greater then 5*
-        //  - **sizeY** is the scail along the Y Axis *this has to be greater then 5*
-        //  - **Layer** is the z depth of the pain. with *0 being the lowest*
-        //
+        /*# pain**(posX, posY, sizeX, sizeY)**
+          - **posX** is the X position of the pain *starting at 0* 
+          - **posY** is the Y position of the pain *starting at 0* 
+          - **sizeX** is the scail along the X Axis *this has to be greater then 5* 
+          - **sizeY** is the scail along the Y Axis *this has to be greater then 5* 
+          - **Layer** is the z depth of the pain. with *0 being the lowest* 
+        */ 
 
         pain(int posX, int posY, int sizeX, int sizeY, int layer);
         
         // text buffer of pain
-        const char* buffer;
+        const char buffer;
+        
+        // This is the id for the pain for the masterPain.
+        int id;
 
         // Position of pain
-        int posX;
-        int posY;
+        int painPosX;
+        int painPosY;
         // size of pain
         int sizeX;
         int sizeY;
+
+        void write(const char* current);
+        void moveCursor(int posX, int posY);
 
         ~pain(){}
     };
 
 
     class masterPain {
+    private:
+        int sizeX;
+        int sizeY;
+
+        void refreshMasterPainSize();
+
     public:
         void update();
         void addPain(pain* addedPain);
         void removePain(int id);
         pain getActivePain();
         pain setActivePain(int id);
+        int getSizeX();
+        int getSizeY();
 
     };
 
+    // These functions need a windows and a linux version
+
     void setup();
     void pos(int x, int y);
-    void write(const char* current, pain pain);
     void setColor(color color);
     pain createPain(int posX, int posY,int sizeX,int sizeY, int layer);
+    void getTerminalSize(int& posXOut, int& posXout);
 
 }
-#endif // !LINUX
-#endif // !CLI_TOOLS_HPP
+#endif // !LINUX 
+#endif // !CLI_TOOLS_HPP 
--- src/linux/cli_tools.cpp
+++ src/linux/cli_tools.cpp
@@ -1,10 +1,36 @@
 #include "cli_tools.hpp"
-#include <cstddef>
+#include <asm-generic/ioctls.h>
+#include <iostream>
+
+// Linux Console imports
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+void clts::pain::write(const char* current) {
+    buffer = current;
+}
+void clts::pain::moveCursor(int posX, int posY) {
+    int screenPositionX = posX + painPosX;
+    int screenPositionY = posY + painPosY;
+    pos(screenPositionX, screenPositionY);
+}
 
 void clts::setup(){}
-void clts::pos(int x, int y){}
-void clts::setColor(clts::color color){}
-void clts::write(const char*, pain pain){}
+void clts::pos(int x, int y){
+    std::cout << "\e["<<x<<";"<<y <<"H";
+}
+void clts::setColor(clts::color color){
+
+}
+
+void clts::getTerminalSize(clts::masterPain masterPain) {
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+}
+
+
 clts::pain clts::createPain(int posX, int posY, int sizeX, int sizeY, int layer){
 return clts::pain(posX,posY,sizeX,sizeY,layer);
 }
